1e48e0eb0e49c29b9c56444d8bb0044268fdcf52
jdjoswald <joswaldleonardo07@gmail.com>
2020-02-10 10:17:45 -0400

    version save status

--- klk2.py
+++ klk2.py
@@ -1,8 +1,13 @@
-import RPi.GPIO as GPIO
-import time
+#import RPi.GPIO as GPIO
+#import time
+f = open ("estado.txt","w")
+f.write("1")
+f.close()
+f = open ("estado.txt","r")
+estado= int(f.read())
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(19,GPIO.OUT)
-GPIO.output(19,True)
+GPIO.output(19,estado)
 
 
